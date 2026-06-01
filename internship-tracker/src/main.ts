e5a03d25d2f7075040c3357c825a8d515af661c3
vClaudio11 <vonclaudio.college@gmail>
2026-06-01 10:49:50 +0800

    feat: add status change event summary counters

--- internship-tracker/src/main.ts
+++ internship-tracker/src/main.ts
@@ -4,12 +4,15 @@ const addButton = document.querySelector("#addApplication") as HTMLButtonElement
 const cancelButton = document.querySelector("#cancel") as HTMLButtonElement;
 const form = document.querySelector(".form") as HTMLFormElement;
 const formInner = document.querySelector(".form-inner") as HTMLDivElement;
-const company = document.querySelector("#company") as HTMLInputElement;
-const jobTitle = document.querySelector("#jobTitle") as HTMLInputElement;
-const location = document.querySelector("#location") as HTMLInputElement;
-const deadline = document.querySelector("#deadline") as HTMLInputElement;
-const status = document.querySelector("#status") as HTMLSelectElement;
+const company = document.querySelector(".company") as HTMLInputElement;
+const jobTitle = document.querySelector(".jobTitle") as HTMLInputElement;
+const location = document.querySelector(".location") as HTMLInputElement;
+const deadline = document.querySelector(".deadline") as HTMLInputElement;
+const status = document.querySelector(".select-status") as HTMLSelectElement;
 const applicationList = document.querySelector("#application-inner") as HTMLUListElement;
+const applied = document.querySelector("#appliedTo-count") as HTMLParagraphElement;
+const inProgress = document.querySelector("#inProgress-count") as HTMLParagraphElement;
+const completed = document.querySelector("#completed-count") as HTMLParagraphElement;
 
 // Define application objects
 type ApplicationStatus = "to apply" | "applied" | "interview" | "offer" | "rejected";
@@ -55,23 +58,59 @@ form.addEventListener('submit', (e) => {
     applications.push(newApplication);
     formInner.classList.add("hidden");
     renderApplications();
-
 });
+
+
 
 function renderApplications(): void {
     // Clear contents of applicationList to prevent duplicates
     applicationList.innerHTML = "";
 
-
-    
+    // for each application in applications, create a new list element with the list properties
     for (const application of applications) {
         const li = document.createElement("li");
+        li.dataset.id = String(application.id);
         li.innerHTML = `<span>${application.company}</span>
                         <span>${application.jobTitle}</span>
                         <span>${application.location}</span>
                         <span>${application.deadline}</span>
-                        <span>${application.status}</span>`;
+                        <select class="select-status">
+                            <option value="to apply">To apply</option>
+                            <option value="applied">Applied</option>
+                            <option value="interview">Interview</option>
+                            <option value="offer">Offer</option>
+                            <option value="rejected">Rejected</option>
+                            <option value="" disabled selected hidden>${application.status}</option>
+                        </select>`;
         applicationList.appendChild(li);
     }
+    updateSummary();
+}
+
+
+
+applicationList.addEventListener('change', (e) => {
+    const target = e.target as HTMLSelectElement;
+    if (target.classList.contains("select-status")) {
+        const li = target.closest("li") as HTMLLIElement;
+        const id = Number(li.dataset.id);
+        let targetList = applications.find(a => a.id === id);
+        if (!targetList) return;
+        targetList.status = target.value as ApplicationStatus;
+        renderApplications() 
+    }
+});
+
+
+function updateSummary(): void {
     
+    // Filter applications with statuses 
+    let inProgressCount = applications.filter(a => a.status === "to apply" || a.status === "applied" || a.status === "interview").length;
+    let completedCount = applications.filter(a => a.status === "offer" || a.status === "rejected").length;
+    let appliedCount = applications.length;
+    
+    // Update values on screen
+    inProgress.textContent = `In progress: ${inProgressCount}`;
+    completed.textContent = `Completed: ${completedCount}`;
+    applied.textContent = `Applied to: ${appliedCount}`;
 }
